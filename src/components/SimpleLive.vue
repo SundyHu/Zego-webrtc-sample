0a3cca5e918e979ce26b8d848838564449553a0b
ryan <hs_18312833057@yeah.net>
2020-06-11 16:45:35 +0800

    test

--- src/components/SimpleLive.vue
+++ src/components/SimpleLive.vue
@@ -105,9 +105,9 @@
                                     this.$refs.previewVideo.autoplay = true;
                                     this.$refs.previewVideo.muted = true;
                                     this.$refs.previewVideo.srcObject = localVideoStream;
-                                    this.$refs.previewVideo.width = 800
-                                    this.$refs.previewVideo.height = 600
-                                    //this.$refs.previewVideo.requestFullscreen();
+                                    //this.$refs.previewVideo.width = 800
+                                    //this.$refs.previewVideo.height = 600
+                                    this.$refs.previewVideo.requestFullscreen();
 
                                     //推流
                                     zg.startPublishingStream('' + new Date().getTime(), localVideoStream);
